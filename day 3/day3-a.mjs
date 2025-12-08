372368f15c4eb8a8a7e6e8c4ffb9219cfed5ccb9
Endre Jordal <endre.jordal@gmail.com>
2025-12-08 08:31:26 +0100

    day 3 updates

--- day 3/day3-a.mjs
+++ day 3/day3-a.mjs
@@ -2,25 +2,22 @@ import fs from "fs";
 
 const data = fs.readFileSync("data.txt", "utf8").split(/\r?\n/);
 
-const testData = fs.readFileSync("testData.txt", "utf8").split(/\r?\n/);
+//const testData = fs.readFileSync("testData.txt", "utf8").split(/\r?\n/);
 
-let totalJoltage = 0;
 for (let i = 0; i < data.length; i++) {
   const digits = data[i].split("");
 
   const bankJoltage = [];
-  let currPos = 0;
+  let currRegion = [...digits.slice(0, bankJoltage.length - 11).map(Number)];
 
   while (bankJoltage.length !== 12) {
-    for (let j = 0; j < digits.length - 1; j++) {
-      if (digits.slice(0, -1).map(Number)[j] > lead) {
-        lead = digits.slice(0, -1).map(Number)[j];
-        leadPos = j;
+    for (let j = 0; j < currRegion.length; j++) {
+      if (currRegion[j] === Math.max(...currRegion)) {
+        bankJoltage.push(currRegion[j]);
+        currRegion = [...currRegion.slice(j, bankJoltage.length - 11)];
       }
     }
   }
-
-  //const trailing = Math.max(...digits.slice(leadPos + 1).map(Number));
-  //totalJoltage += Number(`${lead}${trailing}`);
 }
+const totalJoltage = bankJoltage.reduce((prev, curr) => prev + curr, 0);
 console.log(totalJoltage);
